0ad9b579767bf9738afcf8aadfb06725b10deb9a
DUC HIEU <dtc245200011@gmail.com>
2026-09-25 16:03:15 +0700

    feat(constants): them dinh nghia hang so 7 vai tro nguoi dung

---
+++ src/Ats.Web/Constants/UserRoles.cs
@@ -0,0 +1,12 @@
+namespace Ats.Web.Constants;
+
+public static class UserRoles
+{
+    public const string Candidate = "Candidate";
+    public const string Recruiter = "Recruiter";
+    public const string HiringManager = "HiringManager";
+    public const string Interviewer = "Interviewer";
+    public const string HRManager = "HRManager";
+    public const string Approver = "Approver";
+    public const string Admin = "Admin";
+}
