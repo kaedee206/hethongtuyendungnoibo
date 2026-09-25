375c2af05741a0b2910424a6652c1cf9c77ea0bc
DUC HIEU <dtc245200011@gmail.com>
2026-09-25 16:03:47 +0700

    feat(migration): tao migration AddUserRoleColumn cap nhat cot role vao CSDL

---
+++ src/Ats.Web/Migrations/20260925085618_AddUserRoleColumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ats.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRoleColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
